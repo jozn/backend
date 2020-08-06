7acaba59fb97aa283ab5ee0d7c401325591e2e5c
Hamid <7atashfeshan@gmail.com>
2020-08-06 19:11:56 +0430

    Add base store pb types

--- src/pb/pb/store.rs
+++ src/pb/pb/store.rs
@@ -1,4 +1,4 @@
-// Automatically generated rust module for 'store.proto' file
+// Automatically generated rust module for 'store_v0.proto' file
 
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
