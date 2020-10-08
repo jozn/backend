6cd4a302d91946636ff44e6aa3c0e28e1bbdef64
Hamid <7atashfeshan@gmail.com>
2020-10-08 22:48:28 +0330

    Fix invoke pb serilzing and desreilizing in server/client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,34 +1,59 @@
-use serde::{Deserialize, Serialize};
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(warnings)]
+#![allow(soft_unstable)]
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Act {
-    method: u32,
-    data: Vec<u8>,
-    act_id: u64,
-}
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CheckUsernameParam {
-    id: u64,
-}
+extern crate backbone;
+
+use serde::{Deserialize, Serialize};
+use backbone::{pb};
+use quick_protobuf::{MessageWrite, Writer, BytesReader, MessageRead, deserialize_from_slice};
+// use std::intrinsics::;
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
+    // pb_test();
     me1().await
 }
 
+/*fn main() {
+    // pb_test();
+    pb_test_compact();
+}*/
 async fn me1() -> Result<(), reqwest::Error> {
-    let act = Act {
-        method: 45,
-        // data: "{som:'sdf'}".as_bytes().to_owned(),
-        data: bincode::serialize(&CheckUsernameParam { id: 1894 }).unwrap(),
-        act_id: 15,
+    let data =  pb::ChangePhoneNumberResponse{
+        done: true,
+        text: "a".to_string()
     };
-    let bts = bincode::serialize(&act).unwrap();
+
+    let mut out_bytes = Vec::new();
+    println!("$$$$ aize {:} {}", out_bytes.len() , data.get_size());
+
+    let mut writer = Writer::new(&mut out_bytes);
+    let _result = writer.write_message(&data);
+
+    println!("$$$$ method to send: {:?} {:}", out_bytes , out_bytes.len());
+
+    let act = pb::Invoke{
+        method: 1,
+        action_id: 2,
+        is_response: true,
+        rpc_data: out_bytes,
+        // rpc_data: vec![],
+    };
+
+    let mut out_invoke = Vec::new();
+    let mut writer = Writer::new(&mut out_invoke);
+    let _result = writer.write_message(&act);
+
+    println!("$$$$ bytes to send: {:?}", out_invoke);
+
 
     let new_post = reqwest::Client::new()
         .post("http://127.0.0.1:3000/rpc")
         // .json(&act)
-        .body(bts)
+        .body(out_invoke)
         .send()
         .await?;
 
@@ -37,38 +62,102 @@ async fn me1() -> Result<(), reqwest::Error> {
     Ok(())
 }
 
-async fn sample1() -> Result<(), reqwest::Error> {
-    let new_post = Post {
-        id: None,
-        title: "Reqwest.rs".into(),
-        body: "https://docs.rs/reqwest".into(),
-        user_id: 1,
+
+fn pb_test()  {
+    let data =  pb::ChangePhoneNumberResponse{
+        done: true,
+        text: "a".to_string()
     };
-    let new_post: Post = reqwest::Client::new()
-        .post("https://jsonplaceholder.typicode.com/posts")
-        .json(&new_post)
-        .send()
-        .await?
-        .json()
-        .await?;
 
-    println!("{:#?}", new_post);
-    // Post {
-    //     id: Some(
-    //         101
-    //     ),
-    //     title: "Reqwest.rs",
-    //     body: "https://docs.rs/reqwest",
-    //     user_id: 1
-    // }
-    Ok(())
+    let mut out_bytes = Vec::new();
+    println!("$$$$ aize {:} {}", out_bytes.len() , data.get_size());
+
+    let mut writer = Writer::new(&mut out_bytes);
+    let _result = writer.write_message(&data);
+
+    println!("$$$$ method to send: {:?} {:}", out_bytes , out_bytes.len());
+
+    let act = pb::Invoke{
+        method: 58,
+        action_id: 0,
+        is_response: false,
+        rpc_data: out_bytes,
+        // rpc_data: vec![],
+    };
+
+    let mut out_invoke = Vec::new();
+    let mut writer = Writer::new(&mut out_invoke);
+    let _result = writer.write_message(&act);
+
+    println!("$$$$ bytes to send: {:?}", out_invoke);
+
+
+    // println!("{:#?}", invoke);
+
+
+    let mut bytes_reader: Vec<u8> = vec![];
+    // println!("$$$$ ves got: {:?}", bytes);
+
+    let mut reader = BytesReader::from_bytes(&bytes_reader);
+    let invoke = pb::Invoke::from_reader(&mut reader, &out_invoke);
+
+    if let Ok(act) = &invoke {
+        println!("act {:?}", act);
+        // let pb_bts = server_rpc(act).unwrap_or("vec![]".as_bytes().to_owned());
+        // let pb_bts = rpc::server_rpc(act).unwrap_or("vec![]".as_bytes().to_owned());
+        // return pb_bts;
+    };
+
+    println!("reader: {:#?}", bytes_reader);
+    println!("reader: {:#?}", invoke);
+
+
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Post {
-    id: Option<i32>,
-    title: String,
-    body: String,
-    #[serde(rename = "userId")]
-    user_id: i32,
+fn pb_test_compact()  {
+    let pb_param =  pb::ChangePhoneNumberResponse{
+        done: true,
+        text: "a".to_string()
+    };
+
+    let mut out_bytes = Vec::new();
+    // println!("$$$$ aize {:} {}", out_bytes.len(), pb_param.get_size());
+
+    let _  = Writer::new(&mut out_bytes).write_message(&pb_param);
+    // println!("$$$$ method to send: {:?} {:}", out_bytes , out_bytes.len());
+
+    let invoke = pb::Invoke{
+        method: 58,
+        action_id: 0,
+        is_response: false,
+        rpc_data: out_bytes,
+        // rpc_data: vec![],
+    };
+
+    let mut out_invoke = Vec::new();
+    Writer::new(&mut out_invoke).write_message(&invoke);
+
+    println!("$$$$ bytes to send: {:?}", out_invoke);
+
+    // println!("{:#?}", invoke);
+
+    let mut bytes_reader: Vec<u8> = vec![];
+    // println!("$$$$ ves got: {:?}", bytes);
+
+    let mut invoke = BytesReader::from_bytes(&bytes_reader).read_message::<pb::Invoke>(out_invoke.as_slice());
+    // let invoke = pb::Invoke::from_reader(&mut reader, &out_invoke);
+
+    if let Ok(act) = &invoke {
+        println!("act {:?}", act);
+        // let pb_bts = server_rpc(act).unwrap_or("vec![]".as_bytes().to_owned());
+        // let pb_bts = rpc::server_rpc(act).unwrap_or("vec![]".as_bytes().to_owned());
+        // return pb_bts;
+    };
+
+    println!("reader: {:#?}", bytes_reader);
+    println!("out_invoke: {:#?}", out_invoke);
+    println!("reader: {:#?}", invoke);
+
+
 }
+
