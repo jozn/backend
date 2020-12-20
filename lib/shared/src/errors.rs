b0d04ca62e9bc7c05d1baef221d0f00bb0f8b2b4
Hamid <7atashfeshan@gmail.com>
2020-12-20 09:04:22 +0330

    Migrate error.rs

---
+++ lib/shared/src/errors.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+pub enum GenErr {
+    ReadingPbParam,
+    NoRpcMatch,
+    HttpClientErr(reqwest::Error),
+    ProstDecode(::prost::DecodeError),
+    ProstEncode(::prost::EncodeError),
+}
+
+impl From<reqwest::Error> for GenErr {
+    fn from(e: reqwest::Error) -> Self {
+        GenErr::HttpClientErr(e)
+    }
+}
+
+impl From<prost::DecodeError> for GenErr {
+    fn from(e: ::prost::DecodeError) -> Self {
+        GenErr::ProstDecode(e)
+    }
+}
+
+impl From<prost::EncodeError> for GenErr {
+    fn from(e: ::prost::EncodeError) -> Self {
+        GenErr::ProstEncode(e)
+    }
+}
