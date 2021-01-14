3007e6bb89b2af0e6463a8c8be3c018d5c053f45
Hamid <7atashfeshan@gmail.com>
2021-01-14 21:01:02 +0330

    Add AofFile, AofRow and thread handler

--- micro/play/src/bin/aof.rs
+++ micro/play/src/bin/aof.rs
@@ -102,7 +102,7 @@ impl AofFile {
 
 struct AofFileHandler {
     aof_file: AofFile,
-    sender: std::sync::mpsc::Sender<HandlerCommand>,
+    sender: std::sync::mpsc::SyncSender<HandlerCommand>,
     join: std::thread::JoinHandle<()>,
 }
 
@@ -119,8 +119,10 @@ impl AofFileHandler {
             .write(true)
             .read(true).open(aof_file.full_path()).unwrap();
 
-        // todo make this sized channel buffer
-        let (sender, receiver) = std::sync::mpsc::channel::<HandlerCommand>();
+        // unbufferd channel is considerably faster as it just pushes data to buffer and thread
+        // continues to work: this technique could be a good things for future behaviours when
+        // we need to split or upgrade log files
+        let (sender, receiver) = std::sync::mpsc::sync_channel::<HandlerCommand>(5000);
 
         let s = std::thread::spawn(move || {
             for cmd in receiver {
@@ -152,141 +154,19 @@ struct AofRow {
 
 impl AofRow {
     fn get_file_output(&self) -> Vec<u8> {
-        // let mut out = Vec::with_capacity(110);
         let buff_size = 3 + 8 + self.data.len() + 3;
-        println!("{}",buff_size);
+
         let mut out = [0u8;11].to_vec();
-        // let mut out = Vec::new();
-        // out.resize(buff_size,0);
         byteorder::BE::write_u24(&mut out, self.data.len() as u32);
         byteorder::BE::write_u64(&mut out, self.id);
-        println!("out 1 {}",out.len());
-
         out.write(&self.data);
+
+        // Magic bytes for: 1) easier debugging 2)can look at the end of file to verify end of file
+        // is not corrupted
         out.write(b"\n>\n");
-        println!("out {}",out.len());
 
         assert!(out.len() == buff_size);
 
         out
     }
 }
-//////////////////////////////// Old codes ///////////////////////////
-fn main3_old(){
-    std::fs::create_dir_all("./aof_out").unwrap();
-    // let mut f = std::fs::File::create("./aof_out/1.aof").unwrap();
-    let time = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
-    let filename = format!("./aof_out/{}.aof", time);
-    let mut f = std::fs::OpenOptions::new().create_new(true).write(true).read(true).open(filename).unwrap();
-
-    let msg = shared::pb::Message{
-        gid: 234,
-        by_profile_cid: 23,
-        message_type: 54,
-        text: "aaaaaaaaaaaaaaaaaaa \n dfjlaskdf slskfj lsdk fasdlfjsd fsd aaaaabbbbbbbbbbbbbbbbbbbb".to_string(),
-        via_app_id: 3,
-        seq: 2,
-        edited_time: 2345345234,
-        created_time: 34543534,
-        verified: true,
-        delivery_status: 22,
-        delivery_time: 423,
-        deleted: true,
-        flags: 234,
-        forward: None,
-        reply_to: None,
-        title: "ioioudf fjsd".to_string(),
-        counts: None,
-        setting: None,
-        product: None,
-        files: vec![]
-    };
-
-    // for print
-    let mut buf = vec![];
-    msg.encode(&mut buf);
-    println!("{:?}", buf);
-
-
-    for i in 1..=100000 {
-        let mut buf = vec![];
-        msg.encode(&mut buf);
-
-        let fe = AofRow {
-            id: i as u64,
-            data: buf,
-        };
-
-        f.write(&fe.get_file_output());
-
-        if i%10000 == 0 {
-            println!("{}",i);
-        }
-
-    }
-
-}
-fn main2_bk(){
-    std::fs::create_dir_all("./aof_out").unwrap();
-    // let mut f = std::fs::File::create("./aof_out/1.aof").unwrap();
-    let time = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
-    let filename = format!("./aof_out/{}.aof", time);
-    let mut f = std::fs::OpenOptions::new().create_new(true).write(true).read(true).open(filename).unwrap();
-
-    let msg = shared::pb::Message{
-        gid: 234,
-        by_profile_cid: 23,
-        message_type: 54,
-        text: "aaaaaaaaaaaaaaaaaaa  dfjlaskdf slskfj lsdk fasdlfjsd fsd aaaaabbbbbbbbbbbbbbbbbbbb".to_string(),
-        via_app_id: 3,
-        seq: 2,
-        edited_time: 2345345234,
-        created_time: 34543534,
-        verified: true,
-        delivery_status: 22,
-        delivery_time: 423,
-        deleted: true,
-        flags: 234,
-        forward: None,
-        reply_to: None,
-        title: "ioioudf fjsd".to_string(),
-        counts: None,
-        setting: None,
-        product: None,
-        files: vec![]
-    };
-    let mut buf = vec![];
-    msg.encode(&mut buf);
-    println!("{:?}", buf);
-
-    f.write(&buf);
-
-    let mut buf2 = vec![];
-
-    for i in 1..=100_000 {
-        let mut buf = vec![];
-        msg.encode(&mut buf);
-
-        if i%10000 == 0 {
-            f.write(&buf2);
-            buf2.clear();
-            println!("{}",i);
-        }
-        buf2.write(&buf);
-    }
-
-}
-/*
-
-for i in 1..=1_000_000 {
-        let mut buf = vec![];
-        msg.encode(&mut buf);
-
-        if i%10000 == 0 {
-            f.write(&buf2);
-            buf2.clear();
-            println!("{}",i);
-        }
-        buf2.write(&buf);
-    }
-*/
