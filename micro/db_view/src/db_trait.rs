140f38556cd6b6e05db0e9d40f93ec1a694bf254
Hamid <7atashfeshan@gmail.com>
2021-02-06 18:30:37 +0330

    Add user, profile, direct and chat DB trait to DB

--- micro/db_view/src/db_trait.rs
+++ micro/db_view/src/db_trait.rs
@@ -7,23 +7,19 @@ use shared::{common, common::prost_decode, common::prost_encode, errors::GenErr,
 pub trait DBChannels {
     // =================== Channel ====================
     fn get_channel(&self, channel_id: i64) -> Result<pb::Channel, GenErr>;
-
     fn save_channel(&self, channel: &pb::Channel) -> Result<(), GenErr>;
 
     //fn save_channel_verify(&self, channel: &pb::Channel) -> Result<(), GenErr>;
     // =================== Channel Message ====================
     fn get_channel_message(&self, channel_id: i64, message_id: i64) -> Result<pb::Message, GenErr>;
-
     fn save_channel_message(&self, message: &pb::Message) -> Result<(), GenErr>;
 
     // =================== Channel Follower ====================
     fn get_channel_followers(&self, channel_cid: i64) -> Result<Vec<i64>, GenErr>;
-
     fn save_channel_follower(&self, channel_cid: i64, profile_cid: i64) -> Result<(), GenErr>;
 
     // =================== Channel Message Like ====================
     fn get_message_likes(&self, message_gid: i64) -> Result<Vec<i64>, GenErr>;
-
     fn save_message_like(&self, message_gid: i64, profile_cid: i64) -> Result<(), GenErr>;
 
     // =================== Channel Message Comment ====================
@@ -31,130 +27,57 @@ pub trait DBChannels {
     fn save_message_comment(&self, comment: &pb::Comment) -> Result<(), GenErr>;
 }
 
-pub trait DB: DBChannels {}
+pub trait DBUser {
+    // =================== User ====================
+    fn get_user_by_cid(&self, user_cid: i64) -> Result<pb::User, GenErr>;
+    fn get_user_by_phone(&self, user_cid: i64) -> Result<pb::User, GenErr>;
+    fn save_user(&self, user: &pb::User) -> Result<(), GenErr> ;
+    fn delete_user(&self, user: &pb::User) -> Result<(), GenErr> ;
+
+    // =================== User Session ====================
+    fn get_user_session(&self, user_cid: i64 , session_id :String) -> Result<pb::Session, GenErr>;
+    fn save_user_session(&self, session: &pb::Session) -> Result<(), GenErr> ;
+    fn delete_user_session(&self, session: &pb::Session) -> Result<(), GenErr> ;
+}
+
+pub trait DBProfile {
+    // =================== Profile ====================
+    fn get_profile(&self, profile_cid: i64) -> Result<pb::Profile, GenErr>;
+    fn save_profile(&self, profile: &pb::Profile) -> Result<(), GenErr> ;
+
+    // =================== Profile Following ====================
+    fn get_profile_followings(&self, profile_cid: i64) -> Result<Vec<i64>, GenErr>;
+    // todo this with channel must be in one batch transaction
+    fn save_profile_following(&self, channel_cid: i64, profile_cid: i64) -> Result<(), GenErr>;
+
+    // =================== Profile Contacts ====================
+    fn get_profile_contacts(&self, profile_cid: i64) -> Result<Vec<pb::Contact>, GenErr>;
+    fn save_profile_contacts(&self, contacts: Vec<pb::Contact>) -> Result<(), GenErr>;
+    fn remove_profile_contacts(&self, profile_cid: i64) -> Result<(), GenErr>;
+
+    // =================== Profile Directs ====================
+    fn get_profile_chat_directs(&self, profile_cid: i64) -> Result<Vec<pb::Direct>, GenErr>;
+    fn get_profile_channel_directs(&self, profile_cid: i64) -> Result<Vec<pb::Direct>, GenErr>;
+    fn save_profile_direct(&self, contacts: &pb::Direct) -> Result<(), GenErr>;
+    fn delete_profile_directs(&self, profile_cid: i64, directs: Vec<pb::Direct>) -> Result<(), GenErr>;
+}
+
+trait DBChat {
+    // =================== Chat ====================
+    fn get_chat(&self, profile_id: i64, chat_id: i64) -> Result<pb::Chat, GenErr>;
+    fn save_chat(&self, chat: &pb::Chat) -> Result<(), GenErr> ;
+
+    // =================== Chat Message ====================
+    fn get_chat_message(&self, channel_id: i64, message_id: i64, ) -> Result<pb::Message, GenErr> ;
+    fn save_chat_message(&self, message: &pb::Message) -> Result<(), GenErr>;
+}
+
+
+pub trait DB: DBChannels + DBProfile + DBUser + DBChat  {}
 
 /*
 // Chat Impl
-impl DBCassandra {
-    // =================== Chat ====================
-    fn get_chat(&self, profile_id: i64, chat_id: i64) -> Result<pb::Chat, GenErr> {
-        let sess = &self.session;
 
-        let r = xc::get_chat_by_profile_cid_and_chat_gid(sess, profile_id, chat_id)?;
-        let chat = common::prost_decode(&r.pb_data)?;
 
-        Ok(chat)
-    }
 
-    fn save_chat(&self, chat: &pb::Chat) -> Result<(), GenErr> {
-        let sess = &self.session;
-
-        //todo should be in batch
-
-        let pb = prost_encode(chat)?;
-        // For Profile 1
-        let r1 = xc::Chat {
-            chat_gid: chat.chat_gid as i64,
-            profile_cid: chat.profile1_cid as i64,
-            pb_data: pb.clone(),
-        };
-        r1.save(sess)?;
-
-        // For Profile 2
-        let r2 = xc::Chat {
-            chat_gid: chat.chat_gid as i64,
-            profile_cid: chat.profile2_cid as i64,
-            pb_data: pb,
-        };
-        r2.save(sess)?;
-
-        Ok(())
-    }
-
-    // =================== Chat Message ====================
-    fn get_chat_message(
-        //todo imple
-        &self,
-        channel_id: i64,
-        message_id: i64,
-    ) -> Result<pb::Message, GenErr> {
-        /* let sess = &self.session;
-
-        let r = xc::get_channel_msg_by_channel_id_and_msg_id(sess, channel_id, message_id)?;
-        let ch = common::prost_decode(&r.pb_data)?;
-
-        Ok(ch)*/
-        unimplemented!("")
-    }
-
-    fn save_chat_message(&self, message: &pb::Message) -> Result<(), GenErr> {
-        /*        let sess = &self.session;
-
-        let pb = prost_encode(message)?;
-        let r = xc::ChannelMsg {
-            channel_id: message.cid as i64,
-            msg_id: message.gid as i64,
-            pb_data: pb,
-        };
-        r.save(sess)?;
-
-        Ok(())*/
-        unimplemented!("")
-    }
-}
-
-// Profile Impl
-impl DBCassandra {
-    // =================== Profile ====================
-    fn get_profile(&self, profile_cid: i64) -> Result<pb::Profile, GenErr> {
-        let sess = &self.session;
-
-        let r = xc::get_profile_by_profile_cid(sess, profile_cid)?;
-        let chat = common::prost_decode(&r.pb_data)?;
-
-        Ok(chat)
-    }
-
-    fn save_profile(&self, profile: &pb::Profile) -> Result<(), GenErr> {
-        let sess = &self.session;
-
-        let pb = prost_encode(profile)?;
-        let r = xc::Profile {
-            profile_cid: profile.profile_cid as i64,
-            pb_data: pb,
-        };
-        r.save(sess)?;
-
-        Ok(())
-    }
-
-    // =================== Profile Following ====================
-    fn get_profile_followings(&self, profile_cid: i64) -> Result<Vec<i64>, GenErr> {
-        let sess = &self.session;
-
-        let rows = xc::ProfileFollower_Selector::new()
-            .profile_cid_eq(profile_cid)
-            .get_rows(sess)?;
-        let mut out = vec![];
-        for r in rows {
-            out.push(r.channel_cid);
-        }
-
-        Ok(out)
-    }
-
-    // todo this with channel must be in one batch transaction
-    fn save_profile_following(&self, channel_cid: i64, profile_cid: i64) -> Result<(), GenErr> {
-        let sess = &self.session;
-
-        let r = xc::ProfileFollower {
-            follow_gid: 0, //todo
-            channel_cid,
-            profile_cid,
-        };
-        r.save(sess)?;
-
-        Ok(())
-    }
-}
 */
