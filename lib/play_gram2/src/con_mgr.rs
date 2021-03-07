f4ebd54a04b5d4dd79520d805a6e3705f70459b7
Hamid <7atashfeshan@gmail.com>
2021-03-07 15:25:10 +0330

    Merge play_gram2 into project with removing histroy (41 commits)
    9e5c1f0 (HEAD -> master) Upgrade to support new grammers libaray version 0.2.0 and beyond (from source codes)
    f82059d (origin/master) Merege step 6 into master
    8cc62d7 Add sqlite + change fs to NTFS
    c7f5179 (origin/step_6, step_6) Change files premission due to change the file system to NTFS
    16a555f Add sqlite datebase export
    27a13de Move tg to it's folder moudle with breaking funtinlaity in seprate moudules
    2287931 (origin/step_5, step_5) Add support for extracting urls markup
    3343e87 Add webpage functionlaity + dl thumbs of videos and files
    0e2fbbc Add dl media functionality .
    83c6cf3 Add MeidaThumb and file extension
    2b34d5b Complete image and fiels converter to Media
    b78dc39 Extracting inline chat messages proccessing
    f6c96fe Add crawl next channel
    c3fa3bd Improve username crawling with db integration
    8287571 Improve username crawling
    7e89e96 Add db next username fetcher
    6cd1071 Remove dead codes
    f156c1a (origin/step_4, step_4) Reorginaing tg codes
    c384e4d Add tg get configs
    d686a29 Add general error GenErr
    6b422f6 Add tkanal to sqlite + username validation checking|
    ab72306 cargo fmt
    9c2fd91 Add sqlite and basic sqlite integration 2
    3e3ddc6 Add new tg crawler
    7f36c34 Add tokio sieth crawling username sample
    6c52a00 Add base sample with tokio
    a9d95d3 Add tokio
    ecfa814 (origin/step_3, step_3)  Add crwal, db, pipe, consumer
    d57f0e2 (origin/step_2, step_2) Remove log printing
    5496d4b Add tg2 sample code
    d4c5caa cargo fmt
    13e559d Add session pool basic
    7911ded Extract functionlities to seprate moudles
    bea2952 Add threadpool and mysql carts
    5478c58 Add tkanal sample list of telegram channels
    519698f Add file documents downloader
    86ffd1f Add photo downlader
    329e256 Remove old codes
    a57e468 Add forward header + Implement ChannelInfo
    d097aee Add message convrter and link extracter
    f8ab513 git init: with basic implementaion of telegram
    q

---
+++ lib/play_gram2/src/con_mgr.rs
@@ -0,0 +1,119 @@
+use async_std::task;
+use grammers_client::{Client, Config, SignInError};
+use grammers_mtsender::{AuthorizationError};
+use grammers_session as session;
+use grammers_tl_types as tl;
+use grammers_tl_types::enums::messages::Messages;
+use grammers_tl_types::enums::{Message, MessageEntity};
+use std::sync::{Arc, Mutex};
+use grammers_session::FileSession;
+use std::io;
+use std::io::{Write, BufRead};
+
+// type SharedClient = Arc<Mutex<Client<FileSession>>>;
+
+
+pub async fn get_new_session() -> Result<Client<FileSession>, AuthorizationError> {
+    println!("Connecting to Telegram...");
+    let api_id = 123259;
+    let api_hash = "e88ec58aa1ce01f5630e194e9571d751".to_string();
+    let cf = Config {
+        session: FileSession::load_or_create("./s3.session").unwrap(),//session::Session::load_or_create("./s1.session").unwrap(),
+        api_id: api_id,
+        api_hash: api_hash.clone(),
+        params: Default::default(),
+    };
+    let mut client = Client::connect(cf).await.unwrap();
+    println!("Connected!");
+
+    println!("Sending ping...");
+    dbg!(client.invoke(&tl::functions::Ping { ping_id: 90 }).await?);
+    println!("Ping sent successfully!");
+
+    if !client.is_authorized().await? {
+        println!("Signing in...");
+        // let phone = prompt("Enter your phone number (international format): ")?;
+        let phone = "79620285396";
+        let token = client.request_login_code(&phone, api_id, &api_hash).await.unwrap();
+        let code = prompt("Enter the code you received: ").unwrap();
+        let signed_in = client.sign_in(&token, &code).await;
+        match signed_in {
+            Err(SignInError::PasswordRequired(password_token)) => {
+                // Note: this `prompt` method will echo the password in the console.
+                //       Real code might want to use a better way to handle this.
+                // let hint = password_token.hint().unwrap();
+                // let prompt_message = format!("Enter the password (hint {}): ", &hint);
+                let prompt_message = format!("Enter the password (hint ): ");
+                let password = prompt(prompt_message.as_str()).unwrap();
+
+                client
+                    .check_password(password_token, password.trim())
+                    .await.unwrap();
+            }
+            Ok(_) => (),
+            Err(e) => panic!(e),
+        };
+        println!("Signed in!");
+        match client.session().save() {
+            Ok(_) => {}
+            Err(e) => {
+                println!(
+                    "NOTE: failed to save the session, will sign out when done: {}",
+                    e
+                );
+                // sign_out = true;
+            }
+        }
+    }
+    Ok(client)
+
+    // old code
+    // logig
+/*    if !client.is_authorized().await? {
+        println!("Signing in...");
+        let phone = "79620285396";
+        // let phone = "989338828058";
+        match client.request_login_code(phone, api_id, &api_hash).await {
+            Ok(res) => {
+                println!("write the code form telgeram ....");
+                let s = read();
+                match client.sign_in(&s).await {
+                    Ok(user) => println!("sigin in {:?} ", user),
+                    Err(err) => println!("sigin in error {:?} ", err),
+                    _ => {}
+                }
+            }
+            Err(e) => {
+                println!("error in sending conde: {}", e);
+            }
+        };
+        // client.bot_sign_in(&token, api_id, &api_hash).await?;
+        println!("Signed in!");
+    } else {
+        println!("!! Already Signed in!");
+    }*/
+    // let sc = Arc::new(Mutex::new(client));
+    // Ok(Arc::clone(&sc))
+}
+
+fn read() -> String {
+    let mut input = String::new();
+    match std::io::stdin().read_line(&mut input) {
+        Ok(_) => input.trim().to_string(),
+        Err(e) => panic!("Can not get input value: {:?}", e),
+    }
+}
+
+fn prompt(message: &str) -> Result<String,Box<dyn std::error::Error>> {
+    let stdout = io::stdout();
+    let mut stdout = stdout.lock();
+    stdout.write_all(message.as_bytes())?;
+    stdout.flush()?;
+
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+
+    let mut line = String::new();
+    stdin.read_line(&mut line)?;
+    Ok(line)
+}
---
+++ lib/play_gram2/src/main.rs
@@ -0,0 +1,44 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(warnings)]
+#![allow(soft_unstable)]
+
+#[macro_use]
+extern crate lazy_static;
+
+use async_std::task;
+use grammers_client::{ Client, Config};
+use grammers_session as session;
+use grammers_tl_types as tl;
+use grammers_tl_types::enums::messages::Messages;
+use grammers_tl_types::enums::{Message, MessageEntity};
+
+// mod client_pool;
+mod types;
+
+use std::cell::*;
+
+mod con_mgr;
+mod config;
+mod crawl;
+mod db;
+mod errors;
+// mod tg_old;
+mod utils;
+mod tg;
+
+#[tokio::main]
+async fn main() {
+    // crawl::crawl_next_username().await;
+    // crawl::crawl_config().await;
+    // crawl::crawl_next_channel().await;
+
+    // tg::get_file()
+    for i in 0..1 {
+        println!("{}", 1);
+        // crawl::crawl_next_username().await;
+        let r = crawl::crawl_next_channel_messages().await;
+        println!("{} {:?}", i, r);
+    }
+}
